9b9388cbeacb78758f2cd580c864089325208e32
msanchez123 <marcsanchezborrego@gmail.com>
2025-06-15 19:03:56 +0200

    Cambio en el home del proyecto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,11 @@ const Home = () => {
       <div>
         <h1>Bienvenido a Discos Revolver</h1>
         <p>Explora los mejores discos de rock en nuestra tienda online 🤘</p>
+        <img
+                src={`/assets/tiendaDiscos2.JPG`}
+                alt="tiendaDiscos"
+                style={{ width: "100%", height: "auto", marginBottom: "10px", margin:"auto" }}
+            />
       </div>
     );
   };
